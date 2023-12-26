c7a2007f224ab84b0bb3599fbe61a3b7859048b1
agung <agung@withdipp.com>
2023-12-26 18:30:43 +0800

    add hooks

---
+++ run-hook.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Path to the config.go file
+CONFIG_FILE=$(find $(pwd) -name "config.go" | head -n 1)
+
+# Check if the config.go file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "config.go file not found."
+    exit 1
+fi
+
+# Path to the .env_example file
+ENV_FILE=".env_example"
+touch "$ENV_FILE"
+
+# Remove the existing .env_example file if it exists
+if [ -f "$ENV_FILE" ]; then
+    rm "$ENV_FILE"
+fi
+
+# Extract lines containing 'env' and 'envDefault' tags
+grep -o 'env:"[^"]*"\(\s\+envDefault:"[^"]*"\)\?' "$CONFIG_FILE" | while read -r line ; do
+    echo "$line"
+    # Extract the environment variable name and default value
+    ENV_VAR=$(echo "$line" | grep -o 'env:"[^"]*"' | cut -d '"' -f 2)
+    DEFAULT_VALUE=$(echo "$line" | grep -o 'envDefault:"[^"]*"' | cut -d '"' -f 2)
+
+    # Write to the .env file
+    echo "$ENV_VAR=$DEFAULT_VALUE" >> "$ENV_FILE"
+done
+
+echo ".env_example file has been created successfully."
